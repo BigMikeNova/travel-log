0678dedfd95cee4a49a7a92ae8111bc60980afe0
BigMikeNova <mike.novachek@gmail.com>
2023-07-27 20:08:07 -0400

    updated connection file with URL parser and error catch

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/traveler-db'
-);
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/traveler-db',{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}
+)
+.catch(err => console.log(err));
 
 module.exports = mongoose.connection;
