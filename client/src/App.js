4a7fe4403002c95bbafb8a144a6f0ecfde7f8889
BigMikeNova <mike.novachek@gmail.com>
2023-07-31 19:42:42 -0400

    HomePage.js file, userServices.js for pulling information, updating app.js for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/login';
+import ProfilePage from './pages/profile';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={LoginPage} />
+        <ProtectedRoute path="/home" component={HomePage} />
+        <ProtectedRoute path="/profile" component={ProfilePage} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
