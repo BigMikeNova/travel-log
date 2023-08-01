016459dac635d3987aa94c81f0552f0207985b11
dorianwalker29 <119640290+dorianwalker29@users.noreply.github.com>
2023-07-31 20:56:33 -0400

    login fix

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,47 @@
 // src/Login.js
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Perform form validation (e.g., check for empty fields)
+    if (!email || !password) {
+      alert('Please enter both email and password.');
+      return;
+    }
+
+    try {
+        // Assuming you have a server endpoint for authentication
+        const response = await fetch('/api/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+  
+        const data = await response.json();
+  
+        if (response.ok) {
+          // Authentication successful, redirect to the dashboard or other page
+          history.push('/home');
+        } else {
+          // Authentication failed, display an error message
+          alert(data.message);
+        }
+      } catch (error) {
+        // Handle any network or server-side errors
+        console.error('Error occurred:', error);
+      }
+    };
+
+
   return (
     <body className="bg-gray-10">
       <div className="flex justify-center h-screen w-screen items-center">
