eb648f2e6e61249f86966999a7b377bbbc4bd448
BigMikeNova <mike.novachek@gmail.com>
2023-08-03 18:49:10 -0400

    app.js fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/login';
-import ProfilePage from './pages/profile';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "pages/homePage";
+import LoginPage from "pages/loginPage";
+import ProfilePage from "pages/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
-const App = () => {
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={LoginPage} />
-        <ProtectedRoute path="/home" component={HomePage} />
-        <ProtectedRoute path="/profile/:userId" component={ProfilePage} />
-      </Switch>
-    </Router>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
-};
+}
 
 export default App;
